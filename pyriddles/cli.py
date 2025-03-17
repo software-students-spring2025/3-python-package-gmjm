98db858d01b2bd71a6e556443c65bb25acc7d852
kahmeeah <kao8144@nyu.edu>
2025-03-16 23:06:05 -0400

    initial commit

---
+++ pyriddles/cli.py
@@ -0,0 +1,4 @@
+# this file will handle/parse command line interaction for our package
+
+#TODO: write functions for a CLI parser that allows users to interact with our package via using commands like
+#      'pyriddles --help' or 'pyriddles --difficulty hard' or etc.
---
+++ pyriddles/config.py
@@ -0,0 +1,17 @@
+# this file will store the default settings of our package
+
+DEFAULT_SETTINGS = { # feel free to add, remove, adjust, etc. these as needed
+    "difficulty": "medium",
+    "hint_limit": 3
+}
+
+# TODO: write function to allow users to optionally create a 'settings.json' file in their app to override the default settings.
+#      check first if a 'settings.json' exists and return those values,
+#      otherwise return the DEFAULT_SETTINGS
+#      - handle cases where settings.json has an error ex. incorrect format or key
+def load_settings():
+    return 0
+
+# TODO: optionally, write function to restore user's current settings.json to the default settings (to be used in the cli args, ex. user types 'pyriddles resetsettings')
+def reset_settings():
+    return 0
