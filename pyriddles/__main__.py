2dba2479587776f48ce10c5f874fb10b43ddf7a5
JahleelT <jlt9287@nyu.edu>
2025-03-19 11:51:37 -0400

    took get_riddle() out of hints.py, opened __main__

---
+++ pyriddles/__main__.py
@@ -0,0 +1,36 @@
+import pyriddles.hints as hints
+
+def get_riddle():
+    riddle_id = 0
+    while riddle_id < 1 or riddle_id > 5:
+        answer = input("Choose a number from 1-5 (inclusive): ")
+        if answer.isdigit():
+            riddle_id = int(answer)
+            if 1 <= riddle_id <= 5:
+                break
+            else:
+                print("Try again! That wasn't a number from 1-5!")
+        else:
+            print("That wasn't a number! Please enter a number!")
+    return riddle_id
+
+
+def main():
+    riddles = hints.get_riddles()
+      
+    riddle_id = get_riddle()  
+
+    riddle_data = riddles[riddle_id]  
+    print(f"Riddle {riddle_id}: {riddle_data['riddle']}")
+    
+    user_answer = input("Your answer: ")
+
+    if user_answer.lower() == riddle_data['solution'].lower():
+        print("Correct!")
+    else:
+        print("Incorrect. Here's a hint:")
+        hints.get_hint(riddle_data)  
+    
+
+    if __name__ == "__main__":
+      main()
